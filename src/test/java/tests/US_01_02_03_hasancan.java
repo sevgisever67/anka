09f37bea01cf96bc24cfd3e5d33260b72a889449
canhsan <1979hasancan@gmail.com>
2023-06-10 01:17:30 +0300

    2.10/06/2023

--- src/test/java/tests/US_01_02_03_hasancan.java
+++ src/test/java/tests/US_01_02_03_hasancan.java
@@ -15,6 +15,10 @@ import utilities.TestBaseRapor;
 public class US_01_02_03_hasancan extends TestBaseRapor {
     PackagePage packagePage = new PackagePage();
     MainPage mainPage = new MainPage();
+       Actions actions = new Actions(Driver.getDriver());
+    SoftAssert softAssert = new SoftAssert();
+    String expectedTitle;
+    String actualTitle;
 
     @BeforeMethod
     public void setup() {
@@ -239,6 +243,7 @@ public class US_01_02_03_hasancan extends TestBaseRapor {
         extentTest.pass("Blog sayfasının olduğu görünür");
 
     }
+
     @Test
     public void us03ContactTC_09() {
         Assert.assertTrue(mainPage.contactElementi.isDisplayed());
@@ -265,57 +270,214 @@ public class US_01_02_03_hasancan extends TestBaseRapor {
         //String expectedTitle = "Contact Us";(//div[@class='text-animated'])[9]
         //String actualTitle = Driver.getDriver().getTitle();
         //Assert.assertEquals(actualTitle, expectedTitle);
-       // extentTest.pass("Contact sayfasının olduğu görünür");
+        // extentTest.pass("Contact sayfasının olduğu görünür");
+
+    }
+    @BeforeMethod
+    public void setUp() {
+
+        Driver.getDriver().get(ConfigReader.getProperty("userUrl"));
+        ReusableMethods.waitFor(2);
+        mainPage.cookiesAcceptButton.click();
+    }
+
+    @Test
+    public void LatestBlogTest_TC_39() {
+
+        //actions.moveToElement(Driver.getDriver().findElement(By.xpath("(//h3[.='07'])[1]//..//../..//../a"))).pause(1000).click().perform();
+        actions.moveToElement(mainPage.ourclientYazisi).perform();
+        //actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.waitFor(2);
+
+        mainPage.LatesBlogGreenvillePhotoElement.click();
+
+
+
+        softAssert.assertTrue(mainPage.LatesBlogIstanbulPhotoElement.isDisplayed());
+        softAssert.assertTrue(mainPage.LatesBlogIstanbulPhotoElement.isEnabled());
+        softAssert.assertAll();
+
+        mainPage.LatesBlogIstanbulTextElement.click();
+
+        Driver.getDriver().navigate().back();
+        mainPage.LatesBlogIstanbulReadMoreButton.click();
+
+        actualTitle=Driver.getDriver().getTitle();
+        expectedTitle="Istanbul:A Fusion of East and West";
+        softAssert.assertEquals(actualTitle,expectedTitle);
+        softAssert.assertAll();
 
     }
 
-    public class US_03_HasanCan {
+    @Test
+    public void LatestBlogTest_TC_40(){
 
-        MainPage mainPage = new MainPage();
-        Actions actions = new Actions(Driver.getDriver());
-        SoftAssert softAssert = new SoftAssert();
-        String expectedTitle;
-        String actualTitle;
+        softAssert.assertTrue(mainPage.LatesBlogHittitesPhotoElement.isDisplayed());
+        softAssert.assertTrue(mainPage.LatesBlogHittitesPhotoElement.isEnabled());
+        softAssert.assertAll();
 
-        @BeforeMethod
-        public void setUp() {
+        mainPage.LatesBlogHittitesTextElement.click();
 
-            Driver.getDriver().get(ConfigReader.getProperty("userUrl"));
-        }
+        Driver.getDriver().navigate().back();
+        mainPage.LatesBlogHittitesReadMoreButton.click();
 
-        @Test
-        public void LatestBlogTesti_TC_39() {
-
-            actions.moveToElement(mainPage.LatesBlogIstanbulReadMoreButton).perform();
-            ReusableMethods.waitFor(2);
-            mainPage.LatesBlogIstanbulReadMoreButton.click();
-
-
-
-            softAssert.assertTrue(mainPage.LatesBlogIstanbulPhotoElement.isDisplayed());
-            softAssert.assertTrue(mainPage.LatesBlogIstanbulPhotoElement.isEnabled());
-            softAssert.assertAll();
-
-            mainPage.LatesBlogIstanbulTextElement.click();
-
-            Driver.getDriver().navigate().back();
-            mainPage.LatesBlogIstanbulReadMoreButton.click();
-
-            actualTitle=Driver.getDriver().getTitle();
-            expectedTitle="Istanbul:A Fusion of East and West";
-            softAssert.assertEquals(actualTitle,expectedTitle);
-            softAssert.assertAll();
-
-        }
-
-        @Test
-        public void OurClientTest_TC_46(){
-
-        }
-
-
+        actualTitle=Driver.getDriver().getTitle();
+        expectedTitle="Discovering the Ancient Capital of the Hittites:Bogazkale and Hattusas";
+        softAssert.assertEquals(actualTitle,expectedTitle);
+        softAssert.assertAll();
 
     }
+
+    @Test
+    public void LatestBlogTest_TC_41(){
+
+        softAssert.assertTrue(mainPage.LatesBlogSalinaIslandPhotoElement.isDisplayed());
+        softAssert.assertTrue(mainPage.LatesBlogSalinaIslandPhotoElement.isEnabled());
+        softAssert.assertAll();
+
+        mainPage.LatesBlogSalinaIslandTextElement.click();
+
+        Driver.getDriver().navigate().back();
+        mainPage.LatesBlogSalinaIslandReadMoreButton.click();
+
+        actualTitle=Driver.getDriver().getTitle();
+        expectedTitle="Salina Island: A perfect Family Getaway";
+        softAssert.assertEquals(actualTitle,expectedTitle);
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void LatestBlogTest_TC_42(){
+
+        softAssert.assertTrue(mainPage.LatesBlogBangkokPhotoElement.isDisplayed());
+        softAssert.assertTrue(mainPage.LatesBlogBangkokPhotoElement.isEnabled());
+        softAssert.assertAll();
+
+        mainPage.LatesBlogBangkokTextElement.click();
+
+        Driver.getDriver().navigate().back();
+        mainPage.LatesBlogBangkokReadMoreButton.click();
+
+        actualTitle=Driver.getDriver().getTitle();
+        expectedTitle="Bangkok: A city of Contrasts";
+        softAssert.assertEquals(actualTitle,expectedTitle);
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void LatestBlogTest_TC_43(){
+
+        softAssert.assertTrue(mainPage.LatesBlogBuenosAiresPhotoElement.isDisplayed());
+        softAssert.assertTrue(mainPage.LatesBlogBuenosAiresPhotoElement.isEnabled());
+        softAssert.assertAll();
+
+        mainPage.LatesBlogBuenosAiresTextElement.click();
+
+        Driver.getDriver().navigate().back();
+        mainPage.LatesBlogBuenosAiresReadMoreButton.click();
+
+        actualTitle=Driver.getDriver().getTitle();
+        expectedTitle="Buenos Aires:The Paris of South America";
+        softAssert.assertEquals(actualTitle,expectedTitle);
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void LatestBlogTest_TC_44(){
+
+        softAssert.assertTrue(mainPage.LatesBlogGreenvillePhotoElement.isDisplayed());
+        softAssert.assertTrue(mainPage.LatesBlogGreenvillePhotoElement.isEnabled());
+        softAssert.assertAll();
+
+        mainPage.LatesBlogGreenvilleTextElement.click();
+
+        Driver.getDriver().navigate().back();
+        mainPage.LatesBlogGreenvilleReadMoreButton.click();
+
+        actualTitle=Driver.getDriver().getTitle();
+        expectedTitle="Discovering Greenville: A Hidden Gem in South Carolina";
+        softAssert.assertEquals(actualTitle,expectedTitle);
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void OurClientEasyTicketTest_TC_45(){
+
+        actions.scrollToElement(mainPage.OurClientEasyTicketPhotoElement).perform();
+
+        softAssert.assertTrue(mainPage.OurClientEasyTicketPhotoElement.isDisplayed());
+        softAssert.assertAll();
+
+    }
+    @Test
+    public void OurClientPassportTest_TC_46(){
+
+        actions.scrollToElement(mainPage.OurClientPassportPhotoElement).perform();
+
+        softAssert.assertTrue(mainPage.OurClientPassportPhotoElement.isDisplayed());
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void OurClientIcanActivitiesTest_TC_47(){
+
+        actions.scrollToElement(mainPage.OurClientIcanActivitiesPhotoElement).perform();
+
+        softAssert.assertTrue(mainPage.OurClientIcanActivitiesPhotoElement.isDisplayed());
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void OurClientParkmapTest_TC_48(){
+
+        actions.scrollToElement(mainPage.OurClientParkmapPhotoElement).perform();
+
+        softAssert.assertTrue(mainPage.OurClientParkmapPhotoElement.isDisplayed());
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void OurClientEasyFlyTest_TC_49(){
+
+        actions.scrollToElement(mainPage.OurClientEasyFlyPhotoElement).perform();
+
+        softAssert.assertTrue(mainPage.OurClientEasyFlyPhotoElement.isDisplayed());
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void OurClientBussinesIncreaseTest_TC_50(){
+
+        actions.scrollToElement(mainPage.OurClientBussinesIncreasePhotoElement).perform();
+
+        softAssert.assertTrue(mainPage.OurClientBussinesIncreasePhotoElement.isDisplayed());
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void OurClientYachtseaITest_TC_51(){
+
+        actions.scrollToElement(mainPage.OurClientYachtseaPhotoElement).perform();
+
+        softAssert.assertTrue(mainPage.OurClientYachtseaPhotoElement.isDisplayed());
+        softAssert.assertAll();
+
+    }
+
 
 
 }
+
+
+
+
+
